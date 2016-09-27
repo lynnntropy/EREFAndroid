3142f06bc79e4be012c8fe036fa789188ea8e774
Veselin <omegavesko@gmail.com>
2016-09-28 01:01:02 +0200

    Version name now displayed on about page.

--- EREFAndroid/app/src/main/java/rs/veselinromic/eref/android/fragment/AboutFragment.java
+++ EREFAndroid/app/src/main/java/rs/veselinromic/eref/android/fragment/AboutFragment.java
@@ -1,10 +1,14 @@
 package rs.veselinromic.eref.android.fragment;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import org.sufficientlysecure.htmltextview.HtmlTextView;
 import rs.veselinromic.eref.android.R;
@@ -12,6 +16,7 @@ import rs.veselinromic.eref.android.R;
 public class AboutFragment extends Fragment
 {
     HtmlTextView htmlTextView;
+    TextView version;
 
     public AboutFragment()
     {
@@ -27,7 +32,19 @@ public class AboutFragment extends Fragment
         this.htmlTextView = (HtmlTextView) rootView.findViewById(R.id.htmlTextView);
         this.htmlTextView.setHtmlFromRawResource(getActivity(), R.raw.licenses, false);
 
+        try
+        {
+            PackageInfo pInfo = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0);
+            String versionName = pInfo.versionName;
+
+            this.version = (TextView) rootView.findViewById(R.id.version);
+            this.version.setText("Verzija: " + versionName);
+        }
+        catch (PackageManager.NameNotFoundException e)
+        {
+            Log.e("Version", "e", e);
+        }
+
         return rootView;
     }
-
 }
